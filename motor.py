71795d28d3630289659ac15125bb3626b5d3a596
Maxr1998 <max.rumpf1998@gmail.com>
2023-10-19 22:55:29 +0200

    Set up motor position tracking and add docs

--- motor.py
+++ motor.py
@@ -1,25 +1,73 @@
-from RpiMotorLib import RpiMotorLib
-from config import MOTOR_PINS, SENSOR_PINS
 import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+
+from config import MOTOR_PINS, SENSOR_PINS, STEPS_PER_REVOLUTION
 
 # Setup motor
 motors = RpiMotorLib.BYJMotor("Motors", "28BYJ")
 
+motor_positions = [0, 0, 0]
 
-def motor_clockwise(motor_num, steps=512):
+
+def setup_sensor(sensor_num):
+    """
+    Sets up the sensor as an input with pull-up resistor, thus bringing the pin to LOW when the sensor is triggered.
+    """
+    GPIO.setup(SENSOR_PINS[sensor_num], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+def sensor_status(sensor_num):
+    """
+    Returns the status of the sensor.
+    :return: True if the sensor is triggered, False otherwise.
+    """
+    return GPIO.input(SENSOR_PINS[sensor_num])
+
+
+def motor_clockwise(motor_num, steps=STEPS_PER_REVOLUTION):
+    """
+    Moves the motor clockwise by the given number of steps.
+    """
     motors.motor_run(MOTOR_PINS[motor_num], wait=.001, steps=steps)
+    motor_positions[motor_num] += steps
+    motor_positions[motor_num] %= STEPS_PER_REVOLUTION
 
 
 def reset_motors():
+    """
+    Resets all motors to their default position.
+    """
+    global motor_positions
+
     for motor_num in range(3):
-        GPIO.setup(SENSOR_PINS[motor_num], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        setup_sensor(motor_num)
 
         steps = 0
-        while GPIO.input(SENSOR_PINS[motor_num]) and steps < 512:
+        while sensor_status(motor_num) and steps < STEPS_PER_REVOLUTION:
             motor_clockwise(motor_num=motor_num, steps=1)
             steps += 1
 
-        if steps == 512:  # one full revolution without finding the sensor
+        if steps == STEPS_PER_REVOLUTION:  # one full revolution without finding the sensor
             raise Exception(f"Motor {motor_num} could not be reset to default position")
 
     print(f"All motors reset to default position")
+    motor_positions = [0, 0, 0]
+
+
+def motor_go_to_position(motor_num, target_position):
+    """
+    Moves the motor to the target position, always going clockwise.
+    """
+    if target_position < 0 or target_position >= STEPS_PER_REVOLUTION:
+        raise ValueError(f"Invalid target position: {target_position}")
+
+    current_position = motor_positions[motor_num]
+
+    # No-op if already at target position
+    if current_position == target_position:
+        return
+
+    # Always go clockwise, even if the target position is behind the current position
+    steps = (target_position - current_position) % STEPS_PER_REVOLUTION
+
+    motor_clockwise(motor_num=motor_num, steps=steps)
