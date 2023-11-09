9a6aa664344bec64110504a76b6ef07754e1f820
Maxr1998 <max.rumpf1998@gmail.com>
2023-11-09 02:39:38 +0100

    Calculate location from current events

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import schedule
 from icalendar import Calendar
 
 import calendar_helper
-from config import USER_COUNT
 import motor
-from user_configuration import UserConfiguration
+from calendar_helper import Event
+from config import USER_COUNT
+from user_configuration import UserConfiguration, LocationKeywords
 
 configurations: list[UserConfiguration | None] = [None, None, None]
 calendars: list[Calendar | None] = [None, None, None]
@@ -22,12 +23,33 @@ def refresh_calendars():
         calendars[i] = calendar_helper.load_calendar(i)
 
 
+def calculate_location(configuration: UserConfiguration, events: list[Event]) -> str:
+    locations = list(filter(None, map(lambda event: configuration.map_location(event), events)))
+    locations = sorted(locations, key=LocationKeywords.priority, reverse=True)  # sort locations by priority
+
+    # Return the highest priority location if available, otherwise fall back to location based on working hours
+    return locations[0] if locations else configuration.calculate_fallback_location()
+
+
+def update_locations():
+    for i in range(USER_COUNT):
+        configuration = configurations[i]
+        calendar = calendars[i]
+        current_events = calendar_helper.get_current_events(calendar)
+        location = calculate_location(configuration, current_events)
+        print(f'User {i + 1} is currently at {location}')
+
+        # TODO: update motors
+
+
 if __name__ == '__main__':
     refresh_configurations()
     refresh_calendars()
     motor.reset_motors()
+    update_locations()
 
     schedule.every(10).minutes.do(refresh_calendars)
+    schedule.every(1).minute.do(update_locations)
 
     while True:
         schedule.run_pending()
--- user_configuration.py
+++ user_configuration.py
@@ -1,7 +1,10 @@
-from datetime import time
+from datetime import time, datetime
 
-import pydantic_yaml
+from dateutil.tz import tzlocal
 from pydantic import BaseModel, ValidationError
+from pydantic_yaml import parse_yaml_file_as
+
+from calendar_helper import Event
 
 
 class WorkHours(BaseModel):
@@ -12,10 +15,24 @@ class WorkHours(BaseModel):
 class LocationKeywords(BaseModel):
     home: list[str]
     work: list[str]
-    meeting: list[str]
     lecture: list[str]
+    meeting: list[str]
     conference: list[str]
     vacation: list[str]
+
+    @staticmethod
+    def priority(location: str) -> int:
+        """
+        Returns the priority of a location.
+        """
+        return {
+            'home': 0,
+            'work': 1,
+            'lecture': 2,
+            'meeting': 3,
+            'conference': 4,
+            'vacation': 5,
+        }[location]
 
 
 class UserConfiguration(BaseModel):
@@ -29,10 +46,34 @@ class UserConfiguration(BaseModel):
         """
         filename = f'{user_num + 1}.yaml'
         try:
-            return pydantic_yaml.parse_yaml_file_as(UserConfiguration, filename)
+            return parse_yaml_file_as(UserConfiguration, filename)
         except FileNotFoundError:
             print(f'Missing configuration file {filename}')
             return None
         except ValidationError:
             print(f'Invalid configuration file {filename}')
             return None
+
+    def map_location(self, event: Event) -> str | None:
+        """
+        Maps an event to a location via keywords.
+        """
+        for location, keywords in self.keywords.model_dump().items():
+            for keyword in keywords:
+                summary = event.summary.lower()
+                # Location or keywords matches summary
+                if location.lower() in summary or keyword.lower() in summary:
+                    return location
+
+        return None
+
+    def calculate_fallback_location(self) -> str:
+        """
+        Calculates the fallback location based on the current time and day.
+        """
+        now = datetime.now(tz=tzlocal())
+        working_day = now.weekday() < 5
+        if working_day and self.work_hours.start <= now.time() <= self.work_hours.end:
+            return 'work'
+        else:
+            return 'home'
