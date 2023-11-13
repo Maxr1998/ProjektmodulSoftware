d7998f3907e4eafb24a456bf3a09000edb028020
Maxr1998 <max.rumpf1998@gmail.com>
2023-11-14 00:38:00 +0100

    Tweak test script times

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import schedule
 import calendar_helper
 from config import TZ
 
-current_time = datetime(2023, 10, 18, 8, 0, 0, tzinfo=TZ)
+current_time = datetime(2023, 11, 1, 5, 0, 0, tzinfo=TZ)
 
 
 def mock_get_time():
@@ -14,7 +14,7 @@ def mock_get_time():
 
 def advance_time():
     global current_time
-    delta = 15 if 8 <= current_time.hour < 19 else 60
+    delta = 15 if 7 <= current_time.hour <= 19 else 60
     current_time = current_time + timedelta(minutes=delta)
     print(f"Time advanced to {current_time}")
 
