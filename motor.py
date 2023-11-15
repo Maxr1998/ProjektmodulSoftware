313f2097ec8dff91bdbb2c7152546fe5c8ebe4d2
Maxr1998 <max.rumpf1998@gmail.com>
2023-11-15 13:35:21 +0100

    Add license

--- motor.py
+++ motor.py
@@ -1,3 +1,18 @@
+# Copyright 2023 Max Rumpf
+#
+# This file is part of Weasley Clock.
+#
+# Weasley Clock is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this software.
+# If not, see <https://www.gnu.org/licenses/>.
+
 try:
     import RPi.GPIO as GPIO
     from RpiMotorLib import RpiMotorLib
